202f73b7c6623bfee553e0c1cfdbf0832b389b19
riyo3350g <yechouben@gmail.com>
2025-10-22 16:41:41 +0100

    Add Link to Metafields Page

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -25,6 +25,7 @@ export default function App() {
           Home
         </Link>
         <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/metafields">Metafields</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
